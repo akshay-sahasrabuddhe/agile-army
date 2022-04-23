129b95b5c0a34000184722d7cff5b6ce3d95a3f0
Srikar104 <sgundam@stevens.edu>
2022-04-23 04:19:08 +0000

    Updating home page carousel slides

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const static = express.static(__dirname + '/public');
+const session = require("express-session");
+const static = express.static(__dirname + "/public");
 
-const configRoutes = require('./routes');
+const configRoutes = require("./routes");
 
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 
-app.use('/public', static);
+app.use("/public", static);
 app.use(express.json());
 
-app.engine('handlebars', exphbs({ defaultLayout : 'main' }));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 configRoutes(app);
 
-
-app.listen(3000, () => {
+app.listen(3500, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+  console.log("Your routes will be running on http://localhost:3500");
 });
